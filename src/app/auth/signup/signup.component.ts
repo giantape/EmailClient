dc6d79abe5935987c2550eb19be6882952166ebc
sapinodejs@gmail.com <>
2020-03-14 20:48:12 +0100

    add first repo, and add async validation username

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatchPassword } from '../validators/match-password';
+import { UniqeUsername } from '../validators/uniqe-username';
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+
+  authForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern(/^[a-z0-9]+$/) ], [this.uniqeUsername.validate]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]),
+    passwordConfirmation: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]),
+  }, {validators: [this.matchPassword.validate]});
+
+  constructor(private matchPassword: MatchPassword, private uniqeUsername: UniqeUsername) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log('form SUbmited')
+  }
+}
---
+++ src/app/auth/validators/uniqe-username.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AsyncValidator, FormControl} from '@angular/forms';
+import { map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class UniqeUsername implements AsyncValidator {
+    constructor(private http: HttpClient) {}
+
+    validate = (control: FormControl) => {
+
+        const { value } = control;
+        console.log(this.http);
+        return this.http.post<any>('https://api.angular-email.com/auth/username', {
+            username: value
+        }).pipe(
+            map(() => {
+            //console.log(value);
+            return null;
+        }), catchError((err) => {
+            if (err.error.username) {
+                return of({nonUniqeUsername: true})
+            } else {
+                return of({noConnection: true})
+            }
+        }))
+    }
+}
